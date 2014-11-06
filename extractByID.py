1c26150531d57fc6641cd7eca18288fc695fc155
Matthew Neave <matthewjneave1@gmail.com>
2014-11-06 14:44:54 +0300

    Update extractByID.py

--- extractByID.py
+++ extractByID.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Matthew J Neave 6.5.2014
+# Matthew J Neave. 6.5.2014
 # extract sequences using a list of header IDs
 # input fasta first, then seq IDs, then a name for the output
 # requires biopython
